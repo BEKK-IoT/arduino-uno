978fa20e8bce734882584990ce0c3b72059f159a
Lars Eliassen <lars.eliassen@bekk.no>
2016-10-05 15:38:21 +0200

    Lampen lever

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-import { firebase, five } from 'devices-core';
-const TEAM = 'my-awsome-team-name';
-const fb = new firebase(TEAM);
+import { five } from 'devices-core';
+const TEAM = 'team-lampz';
+const fblight = new Firebase("https://beacon-wallboard.firebaseio.com/");
 const board = new five.Board();
 
 board.on("ready", function() {
+      console.log("Ready");
 
+      var relay = new five.Relay(3);
+      fblight.child('switch').on('value', d => {
+          var data = d.val();
+          console.log(data);
+
+          if (data) {
+              relay.on();
+          } else {
+              relay.off();
+          }
+      });
 });
